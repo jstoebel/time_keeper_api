9ce54c22068dfc522441150a7f7f676e9ef71fe2
Jacob Stoebel <jstoebel@gmail.com>
2018-08-25 17:35:52 -0400

    restore ensure_hash method

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -31,6 +31,19 @@ class GraphqlController < ApplicationController
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
-    # ...code
+    case ambiguous_param
+    when String
+      if ambiguous_param.present?
+        ensure_hash(JSON.parse(ambiguous_param))
+      else
+        {}
+      end
+    when Hash, ActionController::Parameters
+      ambiguous_param
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
   end
 end
